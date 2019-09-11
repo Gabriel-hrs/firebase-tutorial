2496ac15c7bfef89a9d054f2a058da687d6ffde0
Gabriel-hrs <44143105+Gabriel-hrs@users.noreply.github.com>
2019-09-11 10:02:08 -0300

    Testes de dados do formulário

--- projects/principal/src/app/auth/auth.module.ts
+++ projects/principal/src/app/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AuthComponent } from './auth.component';
-
+import { AuthEmailPasswordComponent } from './auth-email-password/auth-email-password.component';
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, AuthEmailPasswordComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
